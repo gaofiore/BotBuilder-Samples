b056faa6cd168078c4952b11de05754a376c02e1
Gabriel Fiorenzano <gfiorenzano@CalgaryChamber.com>
2018-04-04 09:48:01 -0600

    changes to bot

--- Node/sample-knowledgeBot/searchHelpers.js
+++ Node/sample-knowledgeBot/searchHelpers.js
@@ -1,18 +1,66 @@
-module.exports = function () {
-    global.request = require('request');
+const request = require('request');
+const rootQueryString = `https://dynamicsbotsearch.search.windows.net/indexes/azuresql-index/docs?api-version=2016-09-01`;
 
-    global.searchQueryStringBuilder = function (query) {
-        return queryString + query;
-    }
-
-    global.performSearchQuery = function (queryString, callback) {
-        request(queryString, function (error, response, body) {
-            if (!error && response && response.statusCode == 200) {
-                var result = JSON.parse(body);
-                callback(null, result);
-            } else {
-                callback(error, null);
+module.exports = {
+    facetQuery: (facet, callback) => {
+        const options = {
+            url: `${rootQueryString}&facet=${facet}`,
+            headers: {
+                'api-key': `${process.env.AZURE_SEARCH_KEY}`
             }
-        })
+        }
+        request(options, (error, response, body) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                const result = JSON.parse(body);
+                if (!result || !result['@search.facets'] || !result['@search.facets'][facet]) {
+                    // No items for that facet found
+                    callback(null, null);
+                } else {
+                    callback(null, result['@search.facets'][facet]);
+                }
+            }
+        });
+    },
+    filterQuery: (filterName, filterValue, callback) => {
+        const options = {
+            url: `${rootQueryString}&$filter=${filterName} eq '${filterValue}'`,
+            headers: {
+                'api-key': `${process.env.AZURE_SEARCH_KEY}`
+            }
+        }
+        request(options, (error, response, body) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                const result = JSON.parse(body);
+                if (result.value && result.value.length > 0) {
+                    callback(null, result.value);
+                } else {
+                    callback(null, null);
+                }
+            }
+        });
+    },
+    searchQuery: (keyword, callback) => {
+        const options = {
+            url: `${rootQueryString}&search=${keyword}`,
+            headers: {
+                'api-key': `88440318109368A2F7FAEF978EE7E078`
+            }
+        }
+        request(options, (error, response, body) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                const result = JSON.parse(body);
+                if (result && result.value) {
+                    callback(null, result.value);
+                } else {
+                    callback(null, null);
+                }
+            }
+        });
     }
 }
